a6918eaa40702c745a79ca9c64b4a2e8b7290f55
Your Name <tthien@apcs.vn>
2015-02-13 11:43:01 +0700

    init

---
+++ quiz1/R_week1_quiz1/housing for the state of Idaho.R
@@ -0,0 +1,4 @@
+housing_data<-read.csv("getdata-data-ss06hid.csv")
+worth<-subset(housing_data,VAL==24)
+# we see that worth have 53 obs. (In global Environment window, top right)
+#How many properties are worth $1,000,000 or more? answer 53
