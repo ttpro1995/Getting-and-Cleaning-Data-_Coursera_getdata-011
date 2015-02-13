93a38b60cfa9c0f7a086825642beff8819e8a4eb
Your Name <tthien@apcs.vn>
2015-02-13 11:59:05 +0700

    finish install packages, setting up for reading xlsx

---
+++ Practice/Read_excel_file/install require packages.R
@@ -0,0 +1,5 @@
+#install packages and load lib
+install.packages("xlsx") #install once
+install.packages("rJava") #install once
+install.packages("xlsxjars") #install once
+library(xlsx) #load lib to read xlsx 
---
+++ Practice/Read_excel_file/read xlsx.R
@@ -0,0 +1,2 @@
+#read
+data<-read.xlsx2("Grocery_Stores.xlsx",sheetIndex=1,header=TRUE) # read xlsx and save to data
