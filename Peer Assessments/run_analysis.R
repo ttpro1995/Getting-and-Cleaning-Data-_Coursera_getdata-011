7032a4b6199e06e1fcab2a82d448657a8148fcb3
Your Name <tthien@apcs.vn>
2015-02-21 18:57:14 +0700

    peer - extract data

--- Peer Assessments/run_analysis.R
+++ Peer Assessments/run_analysis.R
@@ -1,3 +1,8 @@
+#Thai THien
+#Peer Assessments
+#Getting and Cleaning Data 
+#getdata-011
+
 # Merges the training and the test sets to create one data set.
 
 x_train<-read.table("UCI HAR Dataset/train/X_train.txt")
@@ -20,5 +25,17 @@ dim(subject_test)     #2947    1
 
 
 dim(features)         #561   2
-feature<-as.vector(features[[2]])   #turn feature dataframe into vector
-length(feature)   #561
+features<-as.vector(features[[2]])   #turn feature dataframe into vector
+length(features)   #561
+
+merge_data<-rbind(x_train,x_test)    #merge train set into test set  
+names(merge_data)<-features          #name it column
+
+
+
+
+MeanStd <- grep("mean\\(\\)|std\\(\\)", features)
+#vector contain index of column have mean std in it name 
+
+extract_data<-merge_data[,MeanStd]   #only mean and std
+write.table(extract_data,file="extract_data.txt",row.names=FALSE)
